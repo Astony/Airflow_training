05280584a24e4951f3d9d54f987c463bd1138bdf
Anton Savrushkin <anton_savrushkin@epam.com>
2022-03-19 22:55:45 +0300

    fix end date and task keyword

--- dags/homework2_dag_task2/dag.py
+++ dags/homework2_dag_task2/dag.py
@@ -7,18 +7,18 @@ from airflow.operators.python import PythonOperator
 def my_func(hello, date, **context):
     print(hello)
     print(date)
-    print(context["task_id"])
+    print(context['task'])
 
 
 with DAG(
     "dag",
     schedule_interval="@daily",
     start_date=datetime(day=1, month=1, year=2021),
-    end_date=datetime(day=10, month=1, year=2021),
+    end_date=datetime(day=10, month=1, year=2023),
 ) as dag:
 
     python_task = PythonOperator(
         task_id="python_task",
         python_callable=my_func,
-        op_kwargs={"hello": "Hello World!", "date": "{{ds}}"},
+        op_kwargs={"hello": "Hello World!", "date": "{{execution_date}}"},
     )
